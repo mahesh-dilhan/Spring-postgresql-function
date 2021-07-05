914ad52f692d0b3c7118a1a5ffba7cdf32d43bd9
mahesh <mail.mahesh.lk@gmail.com>
2021-07-05 12:55:05 +0800

    postgresql-uuid

--- app1/storeproc/src/main/java/io/mahesh/springjava/storeproc/Base.java
+++ app1/storeproc/src/main/java/io/mahesh/springjava/storeproc/Base.java
@@ -3,13 +3,18 @@ package io.mahesh.springjava.storeproc;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 @Data
 public class Base {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private UUID baseid;
+
+    @Column
+    private Integer id;
 
     @Column
     private String instrument;
